b7666da74d3609d3de27bf3d0a610b0b22bd1246
D1maB <you@example.com>
2026-04-01 22:55:44 +0300

    project page

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -2,20 +2,21 @@
     :title="$project->title"
 >
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
-        <div>
-            img
-            <img src="{{$project->imgUrl}}" class="">
+    <div class="grid lg:grid-cols-3 gap-6 sm:gap-8 mx-5 my-15">
+        <div class="lg:col-span-2">
+            <img src="{{$project->imgUrl}}" class="object-cover w-full" width="500" heigth="300" alt="{{$project->title}}">
 
-            <div class="description">full description</div>
+            <div class="description py-4">{{$project->description}}</div>
         </div>
-        <div>
-            <h2>Project title: {{$project->title}}</h2>
+        <div class="lg:col-span-1">
+            <h1>{{$project->title}}</h1>
 
-            <p>short description</p>
-
-            <a href="#">Visit website</a>
-            <a href="#">Visit github</a>
+            <p>{{$project->short_description}}</p>
+            
+            <div class="mt-5">
+                <a href="{{$project->website_url}}" class="mb-2 px-4 py-2 text-center block font-medium rounded-lg border border-red-900 text-white bg-red-900 hover:bg-red-700">Visit website</a>
+                <a href="{{$project->github_url}}" class="px-4 py-2 text-center block font-medium rounded-lg border border-gray-900 text-white bg-gray-900 hover:bg-gray-700">Visit github</a>
+            </div>
 
         </div>
     </div>
